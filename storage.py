4d929e81cca9d53e9992217e906f16f9141c4aaf
leobaxa1 <leobaxajr@gmail.com>
2025-04-24 08:07:38 +0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,4 +1,16 @@
+# storage.py
+
 responses = {}
+
+# Questions list
+questions = [
+    "Are you interested in getting Solar Quotation?",
+    "Do you have existing Solar Panels? Yes/NO",
+    "What is your Utility Provider for Electricity?",
+    "What is your Average Electricity Alone?",
+    "Is your Home Type a Single-Family?",
+    "Do you have Home Owner's Association?"
+]
 
 def get_user_data(phone):
     return responses.get(phone, {})
